e6330e7ad8b442765b95d2eea3834d7b849ea558
mkmkaksl <manojkumar@Manojs-MacBook-Pro.local>
2025-03-09 12:27:53 -0500

    m

--- main_project/solar_app/views.py
+++ main_project/solar_app/views.py
@@ -6,26 +6,29 @@ from rest_framework.response import Response
 from .models import SensorData
 import json
 
+every = []
+
 def home(request):
     data = SensorData.objects.all().values()
 
 
     return render(request, "solar_app/index.html", {
-        "data": data
+        "data": every
     })
 
 @api_view(["POST"])
 def receive_sensor_data(request):
     if request.method == "POST":
-        print("Request: ", request)
-        data = json.loads(request.body.decode("utf-8"))
-        print("Data: ", data)
-        sensor_value = data.get("sensor_value")
-        print("Sensor: ", sensor_value)
-        if not sensor_value is None:
-            print("Received: ", sensor_value)
-            SensorData.objects.create(sensor_value=sensor_value)
-            return Response({"message": "Data received"}, status=201)
-        else:
-            print("request none")
-            return Response({"message": "Failed. Received: " + str(sensor_value)}, status=400)
+        # print("Request: ", request)
+        # data = json.loads(request.body.decode("utf-8"))
+        # print("Data: ", data)
+        # sensor_value = data.get("sensor_value")
+        # print("Sensor: ", sensor_value)
+        every.append(request)
+        return Response({"message": "Data received"}, status=201)
+        # if not sensor_value is None:
+        #     print("Received: ", sensor_value)
+        #     SensorData.objects.create(sensor_value=sensor_value)
+        # else:
+        #     print("request none")
+        #     return Response({"message": "Failed. Received: " + str(sensor_value)}, status=400)
