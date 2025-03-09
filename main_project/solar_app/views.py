cf9bef75feb88b8da84b41e95b8108c4a4806fe2
mkmkaksl <manojkumar@Manojs-MacBook-Pro.local>
2025-03-09 12:11:08 -0500

    Check

--- main_project/solar_app/views.py
+++ main_project/solar_app/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import SensorData
+import json
 
 def home(request):
     data = SensorData.objects.all().values()
@@ -16,9 +17,12 @@ def home(request):
 @api_view(["POST"])
 def receive_sensor_data(request):
     if request.method == "POST":
-        sensor_value = request.data.get("sensor_value").decode("utf-8")
+        data = json.loads(request.body.decode("utf-8"))
+        sensor_value = data.get("sensor_value")
         if not sensor_value is None:
+            print("Received: ", sensor_value)
             SensorData.objects.create(sensor_value=sensor_value)
             return Response({"message": "Data received"}, status=201)
         else:
-            return Response({"message": "Failed"}, status=400)
+            print("request none")
+            return Response({"message": "Failed. Received: "}, status=400)
